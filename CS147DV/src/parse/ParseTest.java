b50d2332aeb4c549c730eff8ff981d1f03e77fb6
rdeamici <rtdeamicis@gmail.com>
2020-10-26 02:42:48 -0700

    reverting repo back to state with java program in tact

---
+++ CS147DV/src/parse/ParseTest.java
@@ -0,0 +1,18 @@
+package parse;
+
+/**
+ * 
+ * @author Jordan
+ *
+ *	A class that tests the console capabilities of AssemblyParser
+ */
+public class ParseTest
+{
+
+	public static void main(String[] args)
+	{
+		AssemblyParser.startConsoleParser();
+
+	}
+
+}
