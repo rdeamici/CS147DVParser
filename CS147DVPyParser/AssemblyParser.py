e70574b054e51bed995a43748bbf05ecaf072b67
rdeamici <rtdeamicis@gmail.com>
2020-10-29 22:25:55 -0700

    added more error raising. Added somem custom Exceptions for R-type instructions and chvalidating mneomics

--- CS147DVPyParser/AssemblyParser.py
+++ CS147DVPyParser/AssemblyParser.py
@@ -1,14 +1,41 @@
+#!/usr/bin/env/python
+
 import sys
-import getopt
+import os
+import argparse
+if hasattr(__builtins__, 'raw_input'):
+      input=raw_input
 '''
  * a python implementation of
  * @author Jordan's java project
  *
  *	A class that can parse CS147DV instructions into their hexadecimal value
-'''     
+'''
+
+class MnemonicError(KeyError):
+    def __init__(self, mnemonic, message="invalid mnemonic: "):
+        self.message = message + mnemonic
+        super().__init__(self.message)
+class RtypeError(ValueError):
+    def __init__(self, e, message="invalid R-type Instruction: "):
+        super().__init__(message+str(e))
+
 rtype_mnemonics = ["add", "sub", "mul", "and", "or", "nor", "slt", "sll", "srl","jr"]
 itype_mnemonics = ["addi", "muli", "andi", "ori", "lui", "slti", "beq", "bne", "lw", "sw"]
 jtype_mnemonics = ["jmp" ,"jal" ,"push" ,"pop"]
+numTypes = {
+    'b':2,
+    'bin':2,
+    'binary':2,
+    'h':16,
+    'hex': 16,
+    'hexadecimal':16,
+    'hexidecimal':16,
+    'd':10,
+    'dec':10,
+    'decimal':10,
+    'decamal':10
+}
 functs = {
     "add": "100000",
     "sub": "100010",
@@ -53,39 +80,45 @@ opCodes = {
 }
 
 def x_to_binary(s, base, pad):
-    return bin(int(s,base))[2:].zfill(pad)
-
+    binary = bin(int(s,base))[2:].zfill(pad)
+    assert (len(binary) < pad+2), binary+' is too big len(' + str(len(binary)) +'). max size is '+str(pad)
+    return binary
 def get_funct(mnemonic):
-    try:
+    if mnemonic in functs:
         return functs[mnemonic]
-    except KeyError:
-        raise KeyError('could not get funct code for ' +mnemonic+': invalid mnemonic')
+    else:
+        raise MnemonicError(mnemonic)
+
+def get_base(numType):
+    if numType in numTypes:
+        return numTypes[numType]
+    else:
+        raise ValueError("invalid data Type: "+numType+'\noptions are {0}'.format(numTypes.keys()))
 
 def get_opcode(mnemonic):
-    try:
+    if mnemonic in opCodes:
         return opCodes[mnemonic]
-    except KeyError:
-        raise KeyError('could not get funct code for ' +mnemonic+': invalid mnemonic')
+    else:
+        raise MnemonicError(mnemonic)
 
 def validate_reg_beginning(reg):
     assert reg.lower().startswith('r'), 'register must start with [rR]'
 
 def convert_to_bin(s,padding):
-    # sys.stderr.write('in convert_to_bin\n')
+    # vprint('in convert_to_bin\n')
     # print('converting '+s+' into '+str(padding) + ' bit binary string')
     num = ''
-    try: # try converting to binary
+    try: # have to do binary first, all binary is also valid decimal
         num = int(s,2)
     except:
         # print(s+' is not a binary num')
-        try:
+        try: # have to do decimal next. All decimal is valid hex
             num = int(s)
         except: # try converting to hexadecimal
             try:
-                # print(s+'is not a decimal')
                 num = int(s,16)
             except ValueError:
-                raise ValueError('could not convert '+s+' to a ' + str(padding)+' bit binary string')
+                raise ValueError('could not convert '+s+' to a binary|decimal|hexadecimal number')
     
     # remove first 2 characters from bin(num), which will be '0b'
     # then pad with leading 0s to the size as requested by padding variable
@@ -95,54 +128,68 @@ def convert_to_bin(s,padding):
 
 def parse_rtype(mnemonic,rest):
     # will fail if rest is not 3 items long
+    numType=''
+    base = ''
     try:
-        rd, rs, rt_or_shamt = rest
+        rd, rs, rt_or_shamt,numType = rest
     except ValueError as e:
-        raise ValueError('invalid string for an r-type instruction.\n'+str(e))
+        try:
+            rd,rs,rt_or_shamt = rest
+        except ValueError as e:
+            raise RtypeError(e)
+
     for r in [rd,rs]:
         validate_reg_beginning(r)
-    rd, rs = rd[1:], rs[1:]
-    # print('mnemonic is '+mnemonic)
+
     if mnemonic in ('sll', 'srl'):
         # print('rt_or_shamt = '+rt_or_shamt)
         assert not rt_or_shamt.startswith('r'), 'shift operations require a shamt, not a register'
         shamt = rt_or_shamt
+        if numType: base = get_base(numType)
         rt = '00000'
     else:
         validate_reg_beginning(rt_or_shamt)
-        rt = rt_or_shamt[1:]
+        rt = rt_or_shamt
         shamt = '00000'
-
-    rd, rs = [x_to_binary(r,10,5) for r in [rd,rs]]
-    if rt != '00000': rt = x_to_binary(rt,10,5)
-    if shamt != '00000': shamt = convert_to_bin(shamt,5)
-    return rd, rs, rt, shamt
+    return rd, rs, rt, shamt, base
 
 
 def parse_itype(rest):
-    try:
-        rt, rs, immediate = rest
+    numType = ''
+    base = ''
+    try: #check if user gave a number type
+        rt, rs, immediate, numType = rest
     except ValueError as e:
-        raise ValueError('invalid string for an i-type instruction.\n'+str(e))
+        try:
+            rt, rs, immediate = rest
+        except ValueError as e:
+            raise ValueError('invalid string for an i-type instruction.\n'+str(e))
+    
     for r in [rt, rs]:
         validate_reg_beginning(r)
-    rt, rs = [convert_to_bin(r[1:],5) for r in [rt,rs]]
     imm_err = 'immediate must be a hex|decimal|binary number. Your input: '+immediate
     assert not immediate.lower().startswith('r'),imm_err
-    immediate = convert_to_bin(immediate,16)
-    return rt,rs,immediate
+    # immediate = convert_to_bin(immediate,16)
+    if numType: base = get_base(numType)
+    return rt,rs,immediate, base
 
 def parse_jtype(rest):
+    numType = ''
+    base = ''
     try:
-        address = rest[0]
+        address, numType = rest
     except ValueError as e:
-        raise ValueError('invalid string for a j-type instruction.\n'+str(e))
-    return convert_to_bin(rest, 26)
+        try:
+            address = rest[0]
+        except ValueError as e:
+            raise ValueError('invalid j-type format.\n'+str(e))
+    if numType: base = get_base(numType)
+    return address, base
 
-def convert_bin_to_hex(bin_s):
+def convert_bin_to_hex(bin_s, length):
     try:
-        hex_val = hex(int(bin_s,2))[2:].zfill(8)
-        assert len(hex_val) <= 8, 'hex val is too big'
+        hex_val = hex(int(bin_s,2))[2:].zfill(length)
+        assert len(hex_val) <= length, 'hex val is too big'
     except TypeError:
         raise TypeError('could not convert '+bin_s+ ' to hexadecimal')
     else:
@@ -172,101 +219,171 @@ def usage():
     error += '\n{}'.format(opCodes.keys()) 
     print(error)
     
-def parse_instruction(instruction):
+def parse_instruction(instruction, vprint):
+    assert instruction, "No instruction provided"
+    # define the print function. If user wants to redirect output
+    # send print statements to stderr
+    
     opcode = ''
     rd = ''
     rs = ''
-    rt_or_shamt = ''
     rt = ''
     shamt = ''
     funct = ''
     immediate = ''
     address = ''
-    
-    instruction = instruction.replace(',',' ').split()
-    mnemonic, rest = instruction[0].lower(), instruction[1:]
+
+    components = instruction.lower().replace(',',' ').split()
+    mnemonic, rest = components[0], components[1:]
     opcode = get_opcode(mnemonic)
-    # print('opcode = '+opcode)
-    # print(mnemonic in rtype_mnemonics)
     if mnemonic in rtype_mnemonics:
-        print('rtype')
-        print('<mnemonic> <rd> <rs> <rt|shamt>\n')
-        rd, rs, rt, shamt = parse_rtype(mnemonic,rest)
-        # print('rd = '+rd + '\n\n')
-        # print('rs = '+rs + '\n\n')
-        # print('rt_or_shamt = '+rt_or_shamt + '\n\n')
+        rd, rs, rt, shamt, base = parse_rtype(mnemonic,rest)
         funct = get_funct(mnemonic)
-        # print('funct = '+funct)
+        vprint.write('\nR-Type\n')
+        vprint.write('<mnemonic> <rd> <rs> <rt|shamt> [base]\n')
+        vprint.write('input: '+instruction+'\n')
+        vprint.write(' _____________________________________\n')
+        vprint.write('|opcode| rs  | rt  | rd  |shamt| funct|\n')
+        vprint.write('|______|_____|_____|_____|_____|______|\n\n')
+        if base: shamt = x_to_binary(shamt,base,6)
+        else: shamt = convert_to_bin(shamt,6)
+
     elif mnemonic in itype_mnemonics:
-        print('itype')
-        print('<mnemonic> <rt> <rs> <imm>\n')
-        rt, rs, immediate = parse_itype(rest)
+        rt, rs, immediate, base = parse_itype(rest)
+        vprint.write('\nI-Type\n')
+        vprint.write('<mnemonic> <rt> <rs> <imm> [base]\n')
+        vprint.write('input: '+instruction+'\n')
+        vprint.write(' ___________________________________\n')
+        vprint.write('|opcode| rs  | rt  |   immediate    |\n')
+        vprint.write('|______|_____|_____|________________|\n\n')
+        if base:
+            immediate = x_to_binary(immediate, base, 16)
+        else:
+            immediate = convert_to_bin(immediate, 16)
     else:
-        # print('parsing jtype...')
-        # print(rest)
-        print('jtype')
-        print('<mnemonic> <address>\n')
-        address = parse_jtype(rest[0])
-        # print('done')    
+        if mnemonic not in ('push','pop'):
+            address, base = parse_jtype(rest)
+        else:
+            address = '0x00'
+            base = 16
+        vprint.write('\nJ=Type\n')
+        vprint.write('<mnemonic> <address> [base]\n')
+        vprint.write('input: '+instruction+'\n')
+        vprint.write(' _________________________________\n')
+        vprint.write('|opcode|          address         |\n')
+        vprint.write('|______|__________________________|\n\n')
+        if base: address = x_to_binary(address,base,26)
+        else: address = convert_to_bin(address,26)
     
-    # print('opcode = '+opcode+ '\n\n')
-    # print('function = '+funct + '\n\n')
-    # print('rd = '+rd + '\n\n')
-    # print('rs = '+rs + '\n\n')
-    # print('rt_or_shamt = '+rt_or_shamt + '\n\n')
-    # print('address = '+address + '\n\n')
-    # print('immediate = '+ immediate+ '\n\n')
+    if rd: rd = x_to_binary(rd[1:],10,5)
+    if rt: 
+        rt = x_to_binary(rt[1:],10,5)
+    if rs: rs = x_to_binary(rs[1:],10,5)
+
     to_print = []
     for i, s in [[opcode, 'opcode'],[address,'address'],[rs,'rs'],[rt, 'rt'],[immediate, 'imm'],
               [rd, 'rd'],[shamt,'shamt'],[funct,'funct']]:
         if i:
-            sys.stderr.write(s+'\t')
+            vprint.write(s+'\t')
             to_print.append(i)
-    sys.stderr.write('\n')
+
+    vprint.write('\n')
+    vprint.write('\t'.join(to_print)+'\n\n')
     bin_inst = opcode+address+rs+rt+immediate+rd+shamt+funct
-    print('\t'.join(to_print))
-    print('binary_string')
+    vprint.write('binary_string\n')
     for i in range(len(bin_inst)):
-        if not i or i%4:
-            sys.stderr.write(bin_inst[i])
+        if (i == 0) or (i%4 > 0):
+            vprint.write(bin_inst[i])
         else:
-            sys.stderr.write(' '+bin_inst[i])
-    sys.stderr.write('\n')
-    hex_inst = convert_bin_to_hex(bin_inst) 
+            vprint.write(' '+bin_inst[i])
+    vprint.write('\n')
+    hex_inst = convert_bin_to_hex(bin_inst,8) 
     return hex_inst
 
+
+def interactive_mode(vprint, first_time):
+    if first_time:
+        vprint.write('\nWELCOME TO CS147DV INTERACTIVE INSTRUCTION PARSER!\n')
+        vprint.write('ENTER IN YOUR INSTRUCTIONS ONE AT A TIME\n')
+        vprint.write('THE RESULT WILL BE PRINTED TO THE SCREEN\n')
+        vprint.write('INPUT SHOULD BE OF THE TYPE:\n')
+        vprint.write('  R-Type:  <mnemonic> <rd>,<rs>,<rt|shamt>\n')
+        vprint.write('  I-Type:  <mnemonic> <rt>,<rs>, <imm>\n')
+        vprint.write('  J-Type:  <mnemonic> <address>\n')
+        vprint.write('OUTPUT WILL BE A 32 BIT HEXADECIMAL NUMBER\n')
+        vprint.write('PRESS ctrl-c TO EXIT AT ANY TIME\n\n')
+
+    instruction = input("\nenter your intruction: ")
+    return instruction 
+
 if __name__ == "__main__":
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'h', ["help"])
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        print(str(err))  # will print something like "option -a not recognized"
-        usage()
-        sys.exit(2)
-    for o, a in opts:
-        if o in ('-h', '--help'):
-            usage()
-            sys.exit()
-        else:
-            assert False, 'unhandled option'
+    epilog = 'input: an instruction or set of instructions\n'
+    epilog += '       passed in via the command line, as a file, or interactively.\n\n'
+    epilog += 'instruction types: R-type: <mnemonic> <rd> <rs> <rt|shamt>\n'
+    epilog += '                   I-type: <mnemonic> <rt> <rs> <immediate>\n'
+    epilog += '                   J-type: <mnemonic> <address>\n\n'
+    epilog += 'output: instruction will be parsed into its component parts.\n'
+    epilog += '        By default the:\n'
+    epilog += '        \t- instruction type\n'
+    epilog += '        \t- bit values of each component\n'
+    epilog += '        \t- result in the form of a 32-bit binary string\n'
+    epilog += '        will be output to stderr\n\n'
+    epilog += '        the resulting hexadecimal string will be output to stdout.\n'
+    parser = argparse.ArgumentParser(description="parse CS147DV instructions into hexadecimal",
+                            prog = 'CS147DVInstructionParser',
+                            formatter_class=argparse.RawDescriptionHelpFormatter,
+                            epilog = epilog)
+
+    parser.add_argument('instructions',nargs='*', default=[], help = 'CS147DV instructions wrapped in "quotes"')
+    parser.add_argument('-f','--file', nargs='?',type=argparse.FileType('r'), help='file of CS147DV instructions.')
+    parser.add_argument('-o','--outfile', nargs='?',type=argparse.FileType('w'),
+                        default=sys.stdout, help='File to save results to. Will write over whatever is already in this file')
+    parser.add_argument('-i','--interactive',action='store_true', help='evoke interactive mode.')
+    parser.add_argument('-q','--quiet', action='store_true', help='suppress meta-info about each instruction')
+    args = parser.parse_args()
+    # try:
+    #     opts, args = getopt.getopt(sys.argv[1:], 'hif', ["help",'instructions','file'])
+    # except getopt.GetoptError as err:
+    #     # print help information and exit:
+    #     print(str(err))  # will print something like "option -a not recognized"
+    #     usage()
+    #     sys.exit(2)
+    instructions = args.instructions
+    if args.file:
+        with args.file as f:
+            instructions += [i.strip() for i in f]
     
-    not_done = True
-    while not_done:
-        if args:
-            instruction = " ".join(args)
+    outfile = args.outfile
+    interactive = args.interactive
+    # if they don't provide any input, automatically set interactive to True
+    if not interactive:
+        interactive = not instructions
+
+    devnull = open(os.devnull,'w')
+    verbose = not args.quiet
+    vprint = sys.stdout if verbose else devnull
+
+    for i in instructions:
+        if interactive:
+            try:
+                hex_result = parse_instruction(i,vprint)
+            except Exception as e:
+                sys.stderr.write(str(e))
+                sys.stderr.write('\n')
+            else:
+                outfile.write(hex_result+'\n')
         else:
-            try: # python2
-                instruction = raw_input("enter your intruction (ctrl-c to exit): ")
-            except: #python3
-                instruction = input('enter your instruction(ctrl-c to exit): ')
-        
+            hex_result = parse_instruction(i,vprint)
+            outfile.write(hex_result+'\n')
+    first_time=True
+    while interactive:
+        i = interactive_mode(vprint, first_time)
         try:
-            hex_instructions = parse_instruction(instruction)
-        # hex_instructions = parse_instruction(instruction)
+            hex_result = parse_instruction(i,vprint)
         except Exception as e:
-            print('\n')
-            print(e)
-            print('try again (ctrl-c to exit)\n\n')
+            sys.stderr.write(str(e))
+            sys.stderr.write('\ntry again\n\n')
         else:
-            print('hexadecimal_string')
-            print(hex_instructions)
+            outfile.write(hex_result+'\n\n')
+        first_time = False
+            
